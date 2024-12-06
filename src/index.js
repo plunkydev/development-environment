22db489650433d4e19e5d8924f0cea03e79c99f3
plunkydev <plunkydesign@gmail.com>
2024-12-06 16:06:07 -0500

    Linter standard configurado

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './style.css'
+
+const content = document.getElementById('content')
+const title = document.createElement('h1')
+title.innerText = 'Plantilla PlunkyDev'
+content.appendChild(title)
