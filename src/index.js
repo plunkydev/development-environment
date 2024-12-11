0b9840c866e6758c1ed6f206aceef2cc9e2ede23
plunkydev <plunkydesign@gmail.com>
2024-12-11 14:13:41 -0500

    Reemplazado título por botón con mensaje en src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import './style.css'
 
 const content = document.getElementById('content')
-const title = document.createElement('h1')
-title.innerText = 'Plantilla PlunkyDev'
-content.appendChild(title)
+
+const createButtonWithMessage = () => {
+  const container = Object.assign(document.createElement('div'), {
+    className: 'container',
+    innerHTML: `<button class="button">Hola Plunky</button>`
+  })
+  content.appendChild(container)
+
+  container.firstChild.addEventListener('click', () => {
+    const hola = Object.assign(document.createElement('h1'), {
+      innerText: 'Hola Plunky'
+    })
+    content.appendChild(hola)
+    container.firstChild.style.display = 'none' // Oculta el botón después de hacer clic
+  })
+}
+
+createButtonWithMessage()
